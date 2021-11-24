80b4521a8dadcd5a629c00ead4523a8028f6de79
wobin1 <nathanielmus3@gmail.com>
2021-11-24 02:38:02 +0100

    minor adjustment

--- software/models.py
+++ software/models.py
@@ -1,9 +1,6 @@
 from software import db
 
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+# write model
 
     
--- software/views.py
+++ software/views.py
@@ -1,45 +1,11 @@
-from flask import Blueprint, request, json, jsonify
-from .models import Student
+from flask import Blueprint
+# from .models import name_of_model
 from software import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/register', methods=["GET", "POST"])
-def register():
-    d={}
-    if request.method =="POST":
-        mail = request.form["email"]
-        password = request.form["password"]
-
-        email = Student.query.filter_by(email=mail).first()
-
-        if email is None:
-            register = Student(email=mail, password=password)
-
-            db.session.add(register)
-            db.session.commit()
-           
-            return jsonify(["Register success"])
-        else:
-            # already exist
-            
-            return jsonify(["user alredy exist"])
+@views.route('/')
+def index():
+    return 'hello world'
 
 
-@views.route('/login', methods=["GET", "POST"])
-def login():
-    d = {}
-    if request.method == "POST":
-        mail = request.form["email"]
-        password = request.form["password"]
-
-        login = Student.query.filter_by(email=mail, password=password).first()
-
-        if login is None:
-            # acount not found
-            
-            return jsonify(["Wrong Credentials"]) 
-        else:
-            # acount found
-            
-            return jsonify([ "success"])
